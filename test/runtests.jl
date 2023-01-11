cfc7d04accad8d9adcb732d80aff0145f0ec45ac
David Schlegel <schlegel.david.ds@gmail.com>
2023-01-11 18:47:10 +0100

    fixes

--- test/runtests.jl
+++ test/runtests.jl
@@ -8,7 +8,7 @@ using Test
     hf = FockSpace(:bosonic_mode)
     a₁ = Destroy(hf, Symbol("a",1), 1)
     a₂ = Destroy(hf, Symbol("a",2), 2)
-    a₃ = Destroy(hf, Symbol("a",2), 3)
+    a₃ = Destroy(hf, Symbol("a",3), 3)
 
     #single mode
     H = a₁^2 + (a₁')^2 + a₁ + a₁' + a₁'*a₁
@@ -27,7 +27,7 @@ using Test
     u0 = randn(ComplexF64, 1+dimρ)
     du0 = randn(ComplexF64, 1+dimρ)
     res = zeros(ComplexF64, 1+dimρ)
-    @test_nowarn f_dae(res, u0, du0, 0.0, 0.0)
+    @test_nowarn f_dae(res, du0, u0, 0.0, 0.0)
     @test !iszero(res)
     
 
@@ -51,10 +51,10 @@ using Test
         false
     end
     f_dae = make_ODE_problem(sys)
-    dimρ = (2*(order[1]+1))^2
+    dimρ = prod(2 .* (order .+ 1))^2
     u0 = randn(ComplexF64, 2+dimρ)
     du0 = randn(ComplexF64, 2+dimρ)
     res = zeros(ComplexF64, 2+dimρ)
-    @test_nowarn f_dae(res, u0, du0, 0.0, 0.0)
+    @test_nowarn f_dae(res, du0, u0, 0.0, 0.0)
     @test !iszero(res)
 end
